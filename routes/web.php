c639d34d7d77117b4211cce324667819fb81dd5f
sweet202020 <dolcealessio1@gmail.com>
2023-02-06 10:44:15 +0100

    add routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RestaurantController;
+use App\Http\Controllers\PlateController;
+use App\Http\Controllers\TypeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,10 +26,15 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->group(function () {
+
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('restaurants', RestaurantController::class)->parameters([
+        'restaurants' => 'restaurant:slug'
+    ]);
+    Route::resource('types', TypeController::class)->except(['show', 'create', 'edit']);
+    Route::resource('plates', PlateController::class);
+    Route::resource('order', OrderController::class)->except(['show', 'create', 'edit']);
 });
 
 require __DIR__.'/auth.php';
