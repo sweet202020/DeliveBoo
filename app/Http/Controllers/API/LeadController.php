a229dabfc37ecd88e77d155c356f9a81be32bc18
MaTtiaa01 <mattia2001@live.it>
2023-02-08 11:22:11 +0100

    add api orders

---
+++ app/Http/Controllers/API/LeadController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Lead;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class LeadController extends Controller
+{
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        $validator = Validator::make($data, [
+            'price' => 'required|decimal:2|max:6',
+            'customer_name' => 'required|max:100',
+            'delivery_address' => 'required|max:100',
+            'phone_number' => 'required|max:13'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
+
+        $new_lead = new Lead();
+        $new_lead->fill($data);
+        $new_lead->save();
+
+        return response()->json([
+            'success' => true
+        ]);
+    }
+}
